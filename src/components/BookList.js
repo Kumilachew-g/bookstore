d39e7270124b4438b3a69d1e8e013cab48f70c5a
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-20 20:13:20 +0300

    Import and add useSelector to consume the state

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,16 +1,19 @@
 import React from 'react';
+
+// Import useSelector
 import { useSelector } from 'react-redux';
 import Book from './Book';
 import InputBook from './InputBook';
 
 const BookLists = () => {
+  // Add useSelector to consume the state
   const books = useSelector((state) => state.booksReducer);
   const mapBooks = books.map((book) => (
     <Book key={book.id} id={book.id} title={book.title} author={book.author} />
   ));
 
   return (
-    <div className="book-lists">
+    <div className='book-lists'>
       {mapBooks}
       <InputBook />
     </div>
