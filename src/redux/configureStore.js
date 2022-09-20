3a46416a378458f366646e0265d1044f3bc2347a
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-20 17:55:31 +0300

    Add a reducer to check the status of the category

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import booksReducer from './books/books';
+import bookStatusReducer from './categories/categories';
 
 const reducer = combineReducers({
   booksReducer,
+  bookStatusReducer,
 });
 
 const store = createStore(reducer, applyMiddleware(logger));
