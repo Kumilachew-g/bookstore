43d23fc58e2cfaec99f2f64b5fec6c8082194620
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-21 21:17:59 +0300

    Use React.StrictMode and Import index.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
 import App from './components/App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root'),
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
