a0cab636db0e665d5a64aa30da5e9ce30d3b77e8
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-21 20:48:50 +0300

    Use select tag

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,32 +1,18 @@
 import React, { useState } from 'react';
-import Select from 'react-select';
 import { v4 as uuidv4 } from 'uuid';
-// Import useDispatch
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
-const categories = [
-  { value: '1', label: 'Action' },
-  { value: '2', label: 'Science' },
-  { value: '3', label: 'Economy' },
-  { value: '4', label: 'Fiction' },
-];
+const categories = ['Action', 'Adventure', 'Comedy', 'Drama'];
 
 const InputBook = () => {
-  // Use useDispatch to access the dispatch function
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('Action');
 
-  const handleTitleChange = (e) => {
-    e.preventDefault();
-    setTitle(e.target.value);
-  };
-
-  const handleAuthorChange = (e) => {
-    setAuthor(e.target.value);
-  };
+  const handleTitleChange = (e) => setTitle(e.target.value);
+  const handleCategoryChange = (e) => setCategory(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -34,38 +20,40 @@ const InputBook = () => {
     const newBook = {
       id: uuidv4(),
       title,
-      author,
+      category,
     };
 
     dispatch(addBook(newBook));
     setTitle('');
-    setAuthor('');
+    setCategory('Action');
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h1 style={{ color: '#D9D9D9', padding: '0 50px' }}>ADD NEW BOOK</h1>
-      <div className="form-container">
+      <div className='form-container'>
         <input
-          type="text"
-          className="input-title"
-          id="bookTitle"
-          placeholder="Book Title"
+          type='text'
+          className='input-title'
+          id='bookTitle'
+          placeholder='Book Title'
           value={title}
           onChange={handleTitleChange}
-          name="title"
+          name='title'
         />
-        <input
-          type="text"
-          className="input-title"
-          id="bookAuthor"
-          placeholder="Book Author"
-          value={author}
-          name="author"
-          onChange={handleAuthorChange}
-        />
-        <Select options={categories} className="select" />
-        <input type="submit" value="ADD BOOK" className="add-book" />
+        <select
+          className='select'
+          value={category}
+          onChange={handleCategoryChange}
+          name='category'
+        >
+          {categories.map((categ) => (
+            <option key={uuidv4()} value={categ}>
+              {categ}
+            </option>
+          ))}
+        </select>
+        <input type='submit' value='ADD BOOK' className='add-book' />
       </div>
     </form>
   );
