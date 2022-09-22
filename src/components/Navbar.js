574e92b5875c9748bd733a6a4e731ec54245039b
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-22 10:35:08 +0300

    Fix linter error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import profile from '../assets/images/profile.png'
+import profile from '../assets/images/profile.png';
 
 const Navbar = () => (
-  <nav className='navbar'>
+  <nav className="navbar">
     <h1>Bookstore CMS</h1>
-    <div className='links'>
-      <Link to='/' className='navbar-links'>
+    <div className="links">
+      <Link to="/" className="navbar-links">
         Books
       </Link>
-      <Link to='/categories' className='navbar-links'>
+      <Link to="/categories" className="navbar-links">
         Categories
       </Link>
     </div>
     <div className="profile">
       <img src={profile} alt="profile" />
     </div>
-  </nav>
   </nav>
 );
 
