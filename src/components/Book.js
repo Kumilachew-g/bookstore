43ca2b49a1bec68d84bb3f1c839325916eb501a0
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-22 19:59:36 +0300

    Fix linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,41 +4,41 @@ import RemoveBook from './RemoveBook';
 
 const Book = ({ id, title, category }) => (
   <>
-    <div className='card-styling'>
+    <div className="card-styling">
       <div>
-        <span className='book-title'>{title}</span>
+        <span className="book-title">{title}</span>
         <br />
-        <span className='book-category'>{category}</span>
+        <span className="book-category">{category}</span>
         <br />
-        <span className='book-author'>By Kumilachew</span>
+        <span className="book-author">By Kumilachew</span>
         <br />
-        <button type='button' className='book-btn comments'>
+        <button type="button" className="book-btn comments">
           Comments
         </button>
         <RemoveBook id={id} />
-        <button type='button' className='book-btn'>
+        <button type="button" className="book-btn">
           Edit
         </button>
       </div>
-      <div className='progress-section'>
+      <div className="progress-section">
         <img
-          alt='progress'
+          alt="progress"
           // variant="top"
-          src='https://i.stack.imgur.com/7wcxc.png'
-          className='progress-image'
+          src="https://i.stack.imgur.com/7wcxc.png"
+          className="progress-image"
         />
         <div>
-          <span className='percentage'>28%</span>
+          <span className="percentage">28%</span>
           <br />
-          <span className='completed'>Completed</span>
+          <span className="completed">Completed</span>
         </div>
       </div>
-      <div className='chapter-section'>
-        <span className='current-chapter'>CURRENT CHAPTER</span>
+      <div className="chapter-section">
+        <span className="current-chapter">CURRENT CHAPTER</span>
         <br />
-        <span className='chapter'>Chapter 18</span>
+        <span className="chapter">Chapter 18</span>
         <br />
-        <button type='button' className='update-progress'>
+        <button type="button" className="update-progress">
           UPDATE PROGRESS
         </button>
       </div>
