9b00242af75347cc23d9b4e95f5511beb84bf4ab
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-22 10:08:49 +0300

    Update book page add className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import profile from '../assets/images/profile.png'
 
 const Navbar = () => (
-  <nav className="navbar">
+  <nav className='navbar'>
     <h1>Bookstore CMS</h1>
-    <Link to="/" className="navbar-links">
-      Books
-    </Link>
-    <Link to="/categories" className="navbar-links">
-      Categories
-    </Link>
+    <div className='links'>
+      <Link to='/' className='navbar-links'>
+        Books
+      </Link>
+      <Link to='/categories' className='navbar-links'>
+        Categories
+      </Link>
+    </div>
+    <div className="profile">
+      <img src={profile} alt="profile" />
+    </div>
+  </nav>
   </nav>
 );
 
