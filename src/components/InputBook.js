80390ebb5eb68716141896f1361681cdc9e671f7
Kumilachew-g <kumilachew1212@gmail.com>
2022-09-22 16:54:21 +0300

    Add author in object to fetch the books

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -3,16 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
-const categories = ['Action', 'Adventure', 'Comedy', 'Drama'];
-
 const InputBook = () => {
   const dispatch = useDispatch();
 
   const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('Action');
+  const [author, setAuthor] = useState('');
 
   const handleTitleChange = (e) => setTitle(e.target.value);
-  const handleCategoryChange = (e) => setCategory(e.target.value);
+  const handleAuthorChange = (e) => setAuthor(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,12 +18,13 @@ const InputBook = () => {
     const newBook = {
       id: uuidv4(),
       title,
-      category,
+      author,
+      category: 'Not specified',
     };
 
     dispatch(addBook(newBook));
     setTitle('');
-    setCategory('Action');
+    setAuthor('');
   };
 
   return (
@@ -41,18 +40,16 @@ const InputBook = () => {
           onChange={handleTitleChange}
           name="title"
         />
-        <select
-          className="select input-title"
-          value={category}
-          onChange={handleCategoryChange}
-          name="category"
-        >
-          {categories.map((categ) => (
-            <option key={uuidv4()} value={categ}>
-              {categ}
-            </option>
-          ))}
-        </select>
+        <input
+          type="text"
+          className="input-title"
+          id="bookTitle"
+          placeholder="Book Author"
+          value={author}
+          onChange={handleAuthorChange}
+          name="author"
+        />
+
         <input type="submit" value="ADD BOOK" className="add-book" />
       </div>
     </form>
